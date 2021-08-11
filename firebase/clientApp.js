0fa76ae7aa617c201ecc551f3cc35c1a5a8f5982
bluuweb <contacto@bluuweb.cl>
2021-08-11 19:59:51 -0400

    fin provider

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const clientCredentials = {
-    apiKey: "AIzaSyBEvq4uoQO-owzCS9aeRuC4asKtNfBF4JI",
-    authDomain: "next-prueba-1.firebaseapp.com",
-    projectId: "next-prueba-1",
-    storageBucket: "next-prueba-1.appspot.com",
-    messagingSenderId: "467212682811",
-    appId: "1:467212682811:web:1b98b32d9a25925365be1e",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
--- pages/index.jsx
+++ pages/index.jsx
@@ -13,9 +13,9 @@ export default function Home() {
 
     return (
         <div>
-            <main>
+            <main className="container">
                 <h1>Firebase</h1>
-                <button>Acceder</button>
+                <button className="btn btn-primary">Acceder</button>
             </main>
         </div>
     );
